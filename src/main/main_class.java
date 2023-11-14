27e857bf9cedc666da0d1ad4945bca035eda7f03
mlemingcapoo <ducvu73@gmail.com>
2023-11-14 08:05:18 +0700

    update Util and DAO and mainclass

---
+++ src/main/main_class.java
@@ -0,0 +1,22 @@
+/*
+ * trungpvpp02786
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author catty
+ */
+public class main_class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello");
+        // master changed
+    }
+    
+}
